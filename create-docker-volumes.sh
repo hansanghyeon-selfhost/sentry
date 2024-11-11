ebd50234d7c0c897d9d7bc0435ea2e3ef2388166
999 <999@hyeon.pro>
2024-11-11 15:40:18 +0900

    Create create-docker-volumes.sh

---
+++ create-docker-volumes.sh
@@ -0,0 +1,19 @@
+# .env 파일에서 환경변수 불러오기
+ENV_FILE_PATH="../.env"
+if [ -f "$ENV_FILE_PATH" ]; then
+    export $(grep -v '^#' "$ENV_FILE_PATH" | xargs)
+else
+    echo "$ENV_FILE_PATH 파일을 찾을 수 없습니다."
+    exit 1
+fi
+
+echo "${_group}Creating volumes for persistent storage ..."
+
+echo "Created $(docker volume create --name=sentry-$UNIQUE_KEY-clickhouse)."
+echo "Created $(docker volume create --name=sentry-$UNIQUE_KEY-data)."
+echo "Created $(docker volume create --name=sentry-$UNIQUE_KEY-kafka)."
+echo "Created $(docker volume create --name=sentry-$UNIQUE_KEY-postgres)."
+echo "Created $(docker volume create --name=sentry-$UNIQUE_KEY-redis)."
+echo "Created $(docker volume create --name=sentry-$UNIQUE_KEY-symbolicator)."
+
+echo "${_endgroup}"
